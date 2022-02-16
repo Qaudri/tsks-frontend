8dddf02975e1c7f587709e8fd47d26a8da847d35
Qaudri <aquadirmuhammad@gmail.com>
2022-02-16 20:15:32 +0100

    chore: added company logo

---
+++ components/Ui/Shared/logo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <img :src="require('@/assets/images/logo.svg') " alt="" width="100%">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
