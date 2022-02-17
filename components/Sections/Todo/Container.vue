02e703f508ba8a603e928baee470880749818b1b
Qaudri <aquadirmuhammad@gmail.com>
2022-02-17 13:48:35 +0100

    chore: implemented delete functions

--- components/Sections/Todo/Container.vue
+++ components/Sections/Todo/Container.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <div class="w-full bg-gray-800 py-2 px-10 h-10">
+    <div class="w-full bg-gray-800 py-2 px-10 h-10 hidden">
 
     </div>
 
     <div class="flex h-screen">
 
-      <div class="bg-gray-800 w-1/6 px-4 h-full mx-4">
+      <div class="hidden bg-gray-800 w-1/6 px-4 h-full ">
         <div class="w-60 bg-transparent">
           
         </div>
@@ -15,7 +15,16 @@
 
       <div class="flex items-center justify-center mx-auto">
         <div>
-          <SectionsTodoItem />
+          <SectionsTodoItem id="listItem"/>
+          <SectionsTodoItem id="listItem"/>
+          <SectionsTodoItem id="listItem"/>
+          <SectionsTodoItem id="listItem"/>
+          <SectionsTodoItem id="listItem"/>
+
+          <div>
+            <img :src=" require('@/assets/images/add.svg') " 
+            alt="" class="w-12 h-12 mx-auto" />
+          </div>
         </div>
       </div>
 
@@ -26,7 +35,15 @@
 
 <script>
 export default {
+  data(){
 
+  },
+
+  methods: {
+    addListItem(){
+      this.clone = node.cloneNode(true);
+    }
+  }
 }
 </script>
 
--- components/Sections/Todo/Item.vue
+++ components/Sections/Todo/Item.vue
@@ -1,24 +1,30 @@
 <template>
   <div>
-    <div>
+    <div class="flex items-center group">
 
-        <div class="text-white p-4 bg-gray-800 flex items-center justify-center rounded-xl">
+        <div :class="taskItem ? 'block' : 'hidden' "
+        class="p-4 bg-gray-800 flex items-center justify-center rounded-xl my-6">
 
-          <img :src="require('@/assets/images/checked_checkbox.svg')" alt="" class="h-8 w-8">
+          <img :class="is_completed ? 'block' : 'hidden' " :src="require('@/assets/images/checked_checkbox.svg')" alt="" class="h-8 w-8">
 
-          <div class="h-6 w-6 border-2 border-pink-500 bg-transparent rounded-md">
+          <div :class="is_completed ? 'hidden' : 'block'"  @click="taskComplete" title="Mark as complete"
+          class="h-6 w-6 border-2 border-pink-500 bg-transparent rounded-md cursor-pointer">
             
           </div>
 
-          <div class="text-white font-semibold text-lg mx-4">
+          <div class="text-white font-semibold text-base md:text-lg mx-4">
             Read on Nuxt Js Slot usage.
           </div>
 
-          <div class="ml-20">
+          <div class="md:ml-36 text-white">
             10:00am
           </div>
 
         </div>
+
+        <img @click="deleteItem" :class="taskItem ? 'block' : 'hidden' "
+        :src="require('@/assets/images/delete.svg') " title="Delete this item" 
+        class="mx-4 h-6 w-6 cursor-pointer hidden group-hover:block" />
 
     </div>
   </div>
@@ -26,7 +32,26 @@
 
 <script>
 export default {
+  data(){
+    return{
+      is_completed: false,
+      taskItem: true
+    }
+  },
 
+  methods: {
+    taskComplete(){
+      this.is_completed = !this.is_completed,
+
+      setTimeout(() => {
+        this.taskItem = !this.taskItem
+      }, 1000);
+    },
+
+    deleteItem(){
+      this.taskItem = !this.taskItem
+    }
+  }
 }
 </script>
 
